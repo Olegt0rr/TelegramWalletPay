c00726c8fe52aad4d998e747b064fdf532b8f6e2
Oleg A <t0rr@mail.ru>
2024-04-12 01:49:58 +0300

    🔧 Fixed Updates root model

--- telegram_wallet_pay/schemas/_default.py
+++ telegram_wallet_pay/schemas/_default.py
@@ -1,13 +1,19 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, RootModel
 
 from telegram_wallet_pay.tools import from_snake_to_camel
 
+_model_config = ConfigDict(
+    use_enum_values=True,
+    alias_generator=from_snake_to_camel,
+    populate_by_name=True,
+    validate_assignment=True,
+    arbitrary_types_allowed=True,
+)
+
 
 class DefaultModel(BaseModel):
-    model_config = ConfigDict(
-        use_enum_values=True,
-        alias_generator=from_snake_to_camel,
-        populate_by_name=True,
-        validate_assignment=True,
-        arbitrary_types_allowed=True,
-    )
+    model_config = _model_config
+
+
+class DefaultRootModel(RootModel):
+    model_config = _model_config  # noqa: pyCharm bug
