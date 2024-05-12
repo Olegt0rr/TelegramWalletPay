f58ee257934b8674bb4b5d5aecc33154a5f04eeb
Oleg A <t0rr@mail.ru>
2024-05-13 01:23:12 +0300

    👛 Added wallet emoji

--- telegram_wallet_pay/__init__.py
+++ telegram_wallet_pay/__init__.py
@@ -1,6 +1,15 @@
 __version__ = "0.5.0"
 __api_version__ = "1.2.0"
-__all__ = ["TelegramWalletPay", "enums", "errors", "schemas", "tools"]
+__all__ = [
+    "TelegramWalletPay",
+    "enums",
+    "errors",
+    "schemas",
+    "tools",
+    "WALLET_EMOJI",
+]
 
 from . import enums, errors, schemas, tools
 from .client import TelegramWalletPay
+
+WALLET_EMOJI = "👛"
