510526fd3c20eae5cee2663e6b6bff1a70fa9f9f
Oleg A <t0rr@mail.ru>
2024-05-13 01:39:11 +0300

    👛 Added Wallet button names

--- telegram_wallet_pay/__init__.py
+++ telegram_wallet_pay/__init__.py
@@ -2,6 +2,7 @@ __version__ = "0.5.0"
 __api_version__ = "1.2.0"
 __all__ = [
     "TelegramWalletPay",
+    "constants",
     "enums",
     "errors",
     "schemas",
@@ -9,7 +10,5 @@ __all__ = [
     "WALLET_EMOJI",
 ]
 
-from . import enums, errors, schemas, tools
+from . import constants, enums, errors, schemas, tools
 from .client import TelegramWalletPay
-
-WALLET_EMOJI = "👛"
